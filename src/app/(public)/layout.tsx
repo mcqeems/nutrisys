817cee7e14ca70cc604a79422d5caf3d5f1101c7
Almagribii <brucadalm@gmail.com>
2025-11-16 17:37:21 +0700

    add attribute class

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import PublicNavbar from '@/components/ui/navbar/PublicNavbar';
 import { ThemeProvider } from 'next-themes';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <ThemeProvider defaultTheme="light">
+      <ThemeProvider defaultTheme="light" attribute='class'>
         <PublicNavbar />
         {children}
       </ThemeProvider>
