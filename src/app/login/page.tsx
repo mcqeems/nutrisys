a8878f6f00ec6aec5dde9d899cf60d5927f5e30b
mcqeems <mcqeemsofficial@gmail.com>
2025-11-20 13:56:21 +0700

    added redirect for login and register if logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,18 @@
 import { Metadata } from 'next';
 import LoginPage from './components/LoginPage';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Login - Nutrisys',
   description: 'Nutrisys Login Page',
 };
 
-export default function Login() {
+export default async function Login() {
+  const session = await auth();
+  if (session) {
+    redirect('/dashboard');
+  }
   return (
     <>
       <LoginPage />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,18 @@
 import RegisterPage from './components/RegisterPage';
 import { Metadata } from 'next';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Register - Nutrisys',
   description: 'Nutrisys Register Page',
 };
 
-export default function Register() {
+export default async function Register() {
+  const session = await auth();
+  if (session) {
+    redirect('/dashboard');
+  }
   return (
     <>
       <RegisterPage />
