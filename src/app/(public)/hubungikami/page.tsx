de85631c52125497cf5d18685524d39dfb433a4f
Almagribii <brucadalm@gmail.com>
2025-11-21 16:58:58 +0700

    apply motion frame

--- src/app/(public)/hubungikami/page.tsx
+++ src/app/(public)/hubungikami/page.tsx
@@ -2,15 +2,21 @@
 
 import ContactForm from "./component/contact-form";
 import ContactHeader from "./component/contact-header";
-
+import { motion } from "framer-motion";
 export default function ContactPage() {
   return (
     <main className="min-h-screen bg-backgroundtext-foreground">
-      <ContactHeader />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        <ContactHeader />
 
-      <div className="container mx-auto py-16 px-4 md:px-8 lg:px-16 space-y-16">
-        <ContactForm />
-      </div>
+        <div className="container mx-auto py-16 px-4 md:px-8 lg:px-16 space-y-16">
+          <ContactForm />
+        </div>
+      </motion.div>
     </main>
   );
 }
