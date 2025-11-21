278ce2567cb159d8449a1859d79669f6db419c8f
Almagribii <brucadalm@gmail.com>
2025-11-22 00:14:11 +0700

    update navbar

--- src/components/ui/navbar/PublicNavbar.tsx
+++ src/components/ui/navbar/PublicNavbar.tsx
@@ -1,25 +1,39 @@
-'use client';
-import { useState } from 'react';
-import { PaperAirplaneIcon, MoonIcon, SunIcon, Bars3Icon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { useTheme } from 'next-themes';
+"use client";
+import { useState } from "react";
+import {
+  PaperAirplaneIcon,
+  MoonIcon,
+  SunIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 
 function PublicNavbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
   const { theme, setTheme } = useTheme();
   const toggleTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
-    <div className="fixed top-0 inset-x-0 z-50 bg-transparent ">
+    <div className="fixed top-0 bg-background/10 inset-x-0 z-50 backdrop-blur">
       <nav className="bg-transparent text-foreground border-border">
         <div className="max-w-7xl mx-auto">
           <div className="flex mx-auto justify-between w-5/6 ">
             <div className="flex items-center gap-16 my-6 lg:my-8">
               <div>
-                <Link href="/" className="flex gap-1 font-bold items-center text-foreground">
-                  <PaperAirplaneIcon className="h-6 w-6 text-primary" />
+                <Link
+                  href="/"
+                  className="flex gap-1 font-bold items-center text-foreground"
+                >
+                  <Image
+                    src="/Logo/nutrisys.webp"
+                    width={30}
+                    height={30}
+                    alt="NutriSys Nutrition Analysis"
+                  />
 
                   <div className="flex items-center gap-1.5 font-[1000] leading-none"></div>
                   <div className="-mt-1 hidden text-xl sm:block">
@@ -41,17 +55,29 @@ function PublicNavbar() {
               </div>
               {/* primary */}
               <div className="hidden lg:flex gap-8">
-                <a href="/fitur" className="hover:text-primary transition-colors">
+                <a
+                  href="/fitur"
+                  className="hover:text-primary transition-colors"
+                >
                   Fitur
                 </a>
-                <a href="/chatbot" className="hover:text-primary transition-colors">
+                <a
+                  href="/chatbot"
+                  className="hover:text-primary transition-colors"
+                >
                   Chatbot
                 </a>
-                <a href="/tentang" className="hover:text-primary transition-colors">
+                <a
+                  href="/tentang"
+                  className="hover:text-primary transition-colors"
+                >
                   Tentang
                 </a>
-                <a href="/hubungikami" className="hover:text-primary transition-colors">
-                  Hubungki Kami
+                <a
+                  href="/hubungikami"
+                  className="hover:text-primary transition-colors"
+                >
+                  Hubungi Kami
                 </a>
               </div>
             </div>
@@ -64,11 +90,17 @@ function PublicNavbar() {
                   className="p-2 rounded-full hover:bg-accent hover:text-accent-foreground transition-colors focus:ring-2 focus:ring-ring"
                   aria-label="Toggle Theme"
                 >
-                  {theme === 'dark' ? (
-                    <SunIcon suppressHydrationWarning className="h-6 w-6 text-yellow-400" />
+                  {theme === "dark" ? (
+                    <SunIcon
+                      suppressHydrationWarning
+                      className="h-6 w-6 text-yellow-400"
+                    />
                   ) : (
                     // Di Light Mode, ikon Moon harus terlihat seperti foreground
-                    <MoonIcon suppressHydrationWarning className="h-6 w-6 text-foreground" />
+                    <MoonIcon
+                      suppressHydrationWarning
+                      className="h-6 w-6 text-foreground"
+                    />
                   )}
                 </button>
 
@@ -76,7 +108,6 @@ function PublicNavbar() {
                   {/* Sign In Button */}
                   <Link
                     href="/login"
-                    // Border menggunakan warna border
                     className="rounded-full mr-2 border-solid border-2 border-border py-2 px-6 
                                hover:bg-primary hover:text-primary-foreground transition-colors"
                   >
@@ -85,7 +116,6 @@ function PublicNavbar() {
                   {/* Sign Up Button (Primary Button) */}
                   <Link
                     href="/register"
-                    // Menggunakan warna primary dan foreground primary
                     className="rounded-full mr-2 border-solid border-2 border-primary py-2 px-6 
                                bg-primary text-primary-foreground hover:opacity-80 transition-opacity"
                   >
@@ -107,21 +137,33 @@ function PublicNavbar() {
         <div
           className={`fixed z-40 w-full bg-background overflow-hidden flex flex-col lg:hidden gap-12 
             origin-top duration-700 shadow-xl ${
-              !toggleMenu ? 'h-0' : 'h-full p-6' // Tambahkan padding saat terbuka
+              !toggleMenu ? "h-0" : "h-full p-6" // Tambahkan padding saat terbuka
             }`}
         >
           <div className="px-8">
             <div className="flex flex-col gap-8 font-bold tracking-wider">
-              <a href="/fitur" className="border-l-4 border-primary pl-2">
+              <a
+                href="/fitur"
+                className="border-l-4 border-transparent hover:border-border pl-2"
+              >
                 Fitur
               </a>
-              <a href="/chatbot" className="border-l-4 border-transparent hover:border-border pl-2">
+              <a
+                href="/chatbot"
+                className="border-l-4 border-transparent hover:border-border pl-2"
+              >
                 Chatbot
               </a>
-              <a href="/tentang" className="border-l-4 border-transparent hover:border-border pl-2">
+              <a
+                href="/tentang"
+                className="border-l-4 border-transparent hover:border-border pl-2"
+              >
                 Tentang
               </a>
-              <a href="/hubungikami" className="border-l-4 border-transparent hover:border-border pl-2">
+              <a
+                href="/hubungikami"
+                className="border-l-4 border-transparent hover:border-border pl-2"
+              >
                 Hubungi Kami
               </a>
 
