152704a9be5d6d10d398c93203a3782c8d0b8c78
Almagribii <brucadalm@gmail.com>
2025-11-24 05:58:19 +0700

    apply all on main page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,17 @@
-import HeroSection from "./(home)/hero-section";
+import CTASection from "./(home)/CTASection";
+import HeroSection from "./(home)/HeroSection";
+import Testimonials from "./(home)/Testimonials";
+import WhyNutriSys from "./(home)/WhySection";
+import FaqSection from "./(home)/FaqSection";
 
 function home() {
   return (
     <div>
       <HeroSection />
+      <WhyNutriSys />
+      <Testimonials />
+      <FaqSection />
+      <CTASection />
     </div>
   );
 }
