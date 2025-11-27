29877608d8eedeec62017bf615746cc122d18200
mcqeems <mcqeemsofficial@gmail.com>
2025-11-27 19:43:10 +0700

    migrated

--- src/generated/prisma/internal/class.ts
+++ src/generated/prisma/internal/class.ts
@@ -23,7 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "D:\\Project\\TECHCOMFEST\\nutrisys\\src\\generated\\prisma",
+      "value": "/home/mcqeems/Documents/Project/TECHCOMFEST/nutrisys/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -33,12 +33,12 @@ const config: runtime.GetPrismaClientConfig = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Project\\TECHCOMFEST\\nutrisys\\prisma\\schema.prisma",
+    "sourceFilePath": "/home/mcqeems/Documents/Project/TECHCOMFEST/nutrisys/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../../prisma",
