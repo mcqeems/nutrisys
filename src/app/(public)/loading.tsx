2ffc6f7df3563f525f5b3359e22255afc7c4ea16
mcqeems <mcqeemsofficial@gmail.com>
2025-12-03 15:19:23 +0700

    fixed loader

--- src/app/(public)/loading.tsx
+++ src/app/(public)/loading.tsx
@@ -1,8 +1,13 @@
-'use client'
+'use client';
 
-import Loader from "@/components/Loader";
+import Loader from '@/components/Loader';
+
 const PublicLoadingPage = () => {
-  return <Loader />;
+  return (
+    <div className="relative min-h-[80vh] w-full">
+      <Loader />
+    </div>
+  );
 };
 
 export default PublicLoadingPage;
