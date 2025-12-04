1ec7f70e8fcedd9282192343303402c0aa258be3
mcqeems <mcqeemsofficial@gmail.com>
2025-12-05 03:38:57 +0700

    should be fix

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from './generated/prisma/client';
+// import { PrismaClient } from './generated/prisma/client';
 
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
