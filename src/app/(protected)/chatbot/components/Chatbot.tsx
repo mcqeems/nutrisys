c8c2cd1453c41ca1ad3338496f333579561de54d
mcqeems <mcqeemsofficial@gmail.com>
2026-01-16 09:06:22 +0700

    fix chatbot props

--- src/app/(protected)/chatbot/components/Chatbot.tsx
+++ src/app/(protected)/chatbot/components/Chatbot.tsx
@@ -1,6 +1,6 @@
 import Lottie from "lottie-react";
 import bot from "./bot.json";
 
-export default function Chatbot({ loop }: { loop: boolean }) {
+export default function Chatbot({ loop }: { loop?: boolean }) {
   return <Lottie loop={loop} animationData={bot} />;
 }
